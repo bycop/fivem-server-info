97d308e29fe728f7fc411f0135a6fe6d2cd248fa
bycop <thebycop@gmail.com>
2022-06-30 18:29:34 +0200

    ✨ V2 Change request -> Axios + reformat code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const request = require('request');
-
+const axios = rquire('axios');
 module.exports = {
 	/**
 	* Connected_users function
@@ -8,27 +8,16 @@ module.exports = {
 	* @param {Number} Port Optional server port
 	*/
 	connected_users: function (ip, port) {
-		let promise = new Promise(function (res, rej) {
-			let options = {
-				url: `http://${ip}:${port ? port : 30120}/players.json`,
+		return new Promise(function (res, rej) {
+			axios.get(`http://${ip}:${port ? port : 30120}/players.json`, {
 				timeout: 5000
-			};
-			let data
-			request(options, function (error, response, body) {
-				if (!error && response.statusCode == 200) {
-					try {
-						data = JSON.parse(body);
-						res(data.length)
-					}
-					catch (e) {
-						rej("Error")
-					}
-				}
-				else
-					res("Invalid")
-			})
-		})
-		return promise
+			}).then(function (response) {
+				data = response.data;
+				res(data.length);
+			}).catch(function (error) {
+				rej(error);
+			});
+		});
 	},
 
 	/**
@@ -38,27 +27,16 @@ module.exports = {
 	* @param {Number} Port Optional server port
 	*/
 	max_users: function (ip, port) {
-		let promise = new Promise(function (res, rej) {
-			let options = {
-				url: `http://${ip}:${port ? port : 30120}/info.json`,
+		return new Promise(function (res, rej) {
+			axios.get(`http://${ip}:${port ? port : 30120}/info.json`, {
 				timeout: 5000
-			};
-			let data
-			request(options, function (error, response, body) {
-				if (!error && response.statusCode == 200) {
-					try {
-						data = JSON.parse(body);
-						res(data.vars.sv_maxClients ? data.vars.sv_maxClients : undefined)
-					}
-					catch (e) {
-						rej("Error")
-					}
-				}
-				else
-					res("Invalid")
-			})
-		})
-		return promise
+			}).then(function (response) {
+				data = response.data;
+				res(data.vars.sv_maxClients ? data.vars.sv_maxClients : undefined)
+			}).catch(function (error) {
+				rej(error);
+			});
+		});
 	},
 
 	/**
@@ -68,30 +46,17 @@ module.exports = {
 	* @param {Number} Port Optional server port
 	*/
 	user_list: function (ip, port) {
-		let promise = new Promise(function (res, rej) {
-			let options = {
-				url: `http://${ip}:${port ? port : 30120}/players.json`,
+		return new Promise(function (res, rej) {
+			axios.get(`http://${ip}:${port ? port : 30120}/players.json`, {
 				timeout: 5000
-			};
-			let data
-			request(options, function (error, response, body) {
-				if (!error && response.statusCode == 200) {
-					try {
-						data = JSON.parse(body);
-						return_data = []
-						for (let i = 0; i < data.length; i++) {
-							return_data.push({ "id": data[i].id, "nick": data[i].name, "ping": data[i].ping })
-						}
-						res(return_data)
-					}
-					catch (e) {
-						rej("Error")
-					}
-				}
-				else
-					res("Invalid")
-			})
-		})
-		return promise
+			}).then(function (response) {
+				data = response.data;
+				res(return_data.map(user => {
+					return { id: user.id, nick: user.name, ping: user.ping };
+				}));
+			}).catch(function (error) {
+				rej(error);
+			});
+		});
 	}
 }
